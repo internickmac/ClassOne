76aebdbe3ed012f490d21fa440c50b69b5f10ff5
Jose Trinidad <internick18@gmail.com>
2019-05-20 18:13:50 -0500

    Initial Commit

---
+++ ClassOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassOne
+//
+//  Created by Admin on 20/05/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
